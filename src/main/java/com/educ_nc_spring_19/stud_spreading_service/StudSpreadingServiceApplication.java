8fe1a2523974a7ad2ac82ec410d8ff6fb89ee999
Konstantin Stepanov <kamael-sp@mail.ru>
2019-03-28 10:49:51 +0300

    Initialize project

---
+++ src/main/java/com/educ_nc_spring_19/stud_spreading_service/StudSpreadingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.educ_nc_spring_19.stud_spreading_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudSpreadingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudSpreadingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/educ_nc_spring_19/stud_spreading_service/StudSpreadingServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.educ_nc_spring_19.stud_spreading_service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudSpreadingServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
